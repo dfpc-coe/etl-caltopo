46d4ca12a442402f5ff24bc3a2e0e30bc388ffe5
ingalls <nick@ingalls.ca>
2023-03-06 18:06:34 -0700

    Use hashmap for fewer iters

--- task.js
+++ task.js
@@ -88,18 +88,19 @@ export default class Task extends ETL {
             }
         });
 
-        const features = [];
+        const ids = new Map();
+
         for (const ac of (await res.json()).ac) {
             if (!ac.flight && !ac.r) continue;
 
-            const id = ac.r || ac.flight;
+            const id = (ac.r || ac.flight).toLowerCase().trim();
             const coordinates = [ac.lon, ac.lat];
 
             // If alt. is present convert to meters
             if (!isNaN(parseInt(ac.alt_geom))) coordinates.push(ac.alt_geom * 0.3048);
 
-            const feat = {
-                id: id.trim(),
+            ids.set(id, {
+                id: id,
                 type: 'Feature',
                 properties: {
                     type: 'a-f-A',
@@ -112,46 +113,44 @@ export default class Task extends ETL {
                     type: 'Point',
                     coordinates
                 }
-            };
-
-            features.push(feat);
+            });
         }
 
-        console.log(`ok - fetched ${features.length} planes`);
+        const features = [];
+        for (const include of includes) {
+            const id = include.registration.toLowerCase().trim();
+
+            if (ids.has(id)) {
+                const feat = ids.get(id);
+                if (include.type === 'HELICOPTER') feat.properties.type = 'a-f-A-C-H';
+                if (include.type === 'FIXED WING') feat.properties.type = 'a-f-A-C-F';
+
+                if (include.callsign) feat.properties.callsign = include.callsign;
+
+                if (include.icon) {
+                    feat.properties.icon = include.icon;
+                } else if (include.type === 'HELICOPTER' && include.domain === 'EMS') {
+                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/EMS_ROTOR.png'
+                } else if (include.type === 'HELICOPTER' && include.domain === 'FIRE') {
+                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/FIRE_ROTOR.png'
+                } else if (include.type === 'HELICOPTER' && include.domain === 'LAW') {
+                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/LE_ROTOR.png'
+                } else if (include.type === 'FIXED WING' && include.domain === 'EMS') {
+                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/EMS_FIXED_WING.png'
+                } else if (include.type === 'FIXED WING' && include.domain === 'FIRE') {
+                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/FIRE_AIR_ATTACK.png'
+                } else if (include.type === 'FIXED WING' && include.domain === 'LAW') {
+                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/LE_FIXED_WING.png'
+                }
+
+                features.push(feat);
+            }
+        }
+
+        console.log(`ok - fetched ${ids.size} planes`);
         const fc = {
             type: 'FeatureCollection',
-            features: features.filter((feat) => {
-                for (const include of includes) {
-                    if (
-                        (include.callsign && feat.properties.callsign.toLowerCase() === include.callsign.toLowerCase())
-                        || (include.registration && feat.properties.registration.toLowerCase() === include.registration.toLowerCase())
-                    ) {
-                        if (include.type === 'HELICOPTER') feat.properties.type = 'a-f-A-C-H';
-                        if (include.type === 'FIXED WING') feat.properties.type = 'a-f-A-C-F';
-
-                        if (include.callsign) feat.properties.callsign = include.callsign;
-
-                        if (include.icon) {
-                            feat.properties.icon = include.icon;
-                        } else if (include.type === 'HELICOPTER' && include.domain === 'EMS') {
-                            feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/EMS_ROTOR.png'
-                        } else if (include.type === 'HELICOPTER' && include.domain === 'FIRE') {
-                            feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/FIRE_ROTOR.png'
-                        } else if (include.type === 'HELICOPTER' && include.domain === 'LAW') {
-                            feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/LE_ROTOR.png'
-                        } else if (include.type === 'FIXED WING' && include.domain === 'EMS') {
-                            feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/EMS_FIXED_WING.png'
-                        } else if (include.type === 'FIXED WING' && include.domain === 'FIRE') {
-                            feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/FIRE_AIR_ATTACK.png'
-                        } else if (include.type === 'FIXED WING' && include.domain === 'LAW') {
-                            feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/LE_FIXED_WING.png'
-                        }
-
-                        return true;
-                    }
-                }
-                return false;
-            })
+            features
         };
 
         await this.submit(fc);
