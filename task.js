6f382dde24cd25cae60a3ff93ae51e86896451f3
ingalls <nick@ingalls.ca>
2023-03-20 14:42:51 -0600

    Dump GeoJSON

--- task.js
+++ task.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import ETL from '@tak-ps/etl';
+import EsriDump from 'esri-dump';
 
 try {
     const dotfile = new URL('.env', import.meta.url);
@@ -15,50 +16,11 @@ export default class Task extends ETL {
     static schema() {
         return {
             type: 'object',
-            required: ['ADSBX_TOKEN'],
+            required: ['ARCGIS_URL'],
             properties: {
-                'ADSBX_TOKEN': {
+                'ARCGIS_URL': {
                     type: 'string',
-                    description: 'API Token for ADSBExachange'
-                },
-                'ADSBX_INCLUDES': {
-                    type: 'array',
-                    display: 'table',
-                    description: 'Limit resultant features to a given list of ids',
-                    items: {
-                        type: 'object',
-                        properties: {
-                            domain: {
-                                type: 'string',
-                                description: 'Public Safety domain of the Aircraft',
-                                enum: ['EMS', 'FIRE', 'LAW']
-                            },
-                            agency: {
-                                type: 'string',
-                                description: 'Agency in control of the Aircraft'
-                            },
-                            callsign: {
-                                type: 'string',
-                                description: 'Callsign of the Aircraft'
-                            },
-                            registration: {
-                                type: 'string',
-                                description: 'Registration Number of the Aircraft'
-                            },
-                            type: {
-                                type: 'string',
-                                description: 'Type of Aircraft',
-                                enum: [
-                                    'HELICOPTER',
-                                    'FIXED WING'
-                                ]
-                            },
-                            icon: {
-                                type: 'string',
-                                description: 'Optional TAK Custom Icon'
-                            }
-                        }
-                    }
+                    description: 'ArcGIS MapServer URL to pull data from'
                 },
                 'DEBUG': {
                     type: 'boolean',
@@ -72,113 +34,22 @@ export default class Task extends ETL {
     async control() {
         const layer = await this.layer();
 
-        if (!layer.data.environment.ADSBX_TOKEN) throw new Error('No ADSBX API Token Provided');
-        if (!layer.data.environment.ADSBX_INCLUDES) layer.data.environment.ADSBX_INCLUDES = '[]';
+        if (!layer.data.environment.ARCGIS_URL) throw new Error('No ArcGIS_URL Provided');
 
-        const token = layer.data.environment.ADSBX_TOKEN;
-        const includes = layer.data.environment.ADSBX_INCLUDES;
-        const api = 'https://adsbexchange.com/api/aircraft/v2/lat/40.14401/lon/-119.81204/dist/2650/';
+        const dumper = new ESRIDump(this.layer.environment.ARCGIS_URL);
 
-        const url = new URL(api);
-        url.searchParams.append('apiKey', token);
-        url.searchParams.append('cacheBuster', +new Date());
+        dumper.fetch();
 
-        const res = await fetch(url, {
-            headers: {
-                'api-auth': token
-            }
+        return new Promise((resolve, reject) => {
+            dumper
+                .on('feature', (feature) => {
+                    console.log(feature);
+                })
+                .on('error', reject)
+                .on('done', () => {
+                    console.error('done');
+                });
         });
-
-        const ids = new Map();
-
-        for (const ac of (await res.json()).ac) {
-            if (!ac.flight && !ac.r) continue;
-
-            const id = (ac.r || ac.flight).toLowerCase().trim();
-            const coordinates = [ac.lon, ac.lat];
-
-            // If alt. is present convert to meters
-            if (!isNaN(parseInt(ac.alt_geom))) coordinates.push(ac.alt_geom * 0.3048);
-
-            ids.set(id, {
-                id: id,
-                type: 'Feature',
-                properties: {
-                    type: 'a-f-A',
-                    registration: (ac.r || '').trim(),
-                    callsign: (ac.flight || '').trim(),
-                    time: new Date(),
-                    start: new Date(),
-                    squak: ac.squak,
-                    emergency: ac.emergency,
-                    speed: ac.gs * 0.514444 || 9999999.0,
-                    course: ac.track || 9999999.0
-                },
-                geometry: {
-                    type: 'Point',
-                    coordinates
-                }
-            });
-        }
-
-        const features = [];
-        const features_ids = new Set();
-        for (const include of includes) {
-            const id = include.registration.toLowerCase().trim();
-
-            if (ids.has(id)) {
-                const feat = ids.get(id);
-                if (include.type === 'HELICOPTER') feat.properties.type = 'a-f-A-C-H';
-                if (include.type === 'FIXED WING') feat.properties.type = 'a-f-A-C-F';
-
-                if (include.callsign) feat.properties.callsign = include.callsign;
-
-                if (include.icon) {
-                    feat.properties.icon = include.icon;
-                } else if (include.type === 'HELICOPTER' && include.domain === 'EMS') {
-                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/EMS_ROTOR.png'
-                } else if (include.type === 'HELICOPTER' && include.domain === 'FIRE') {
-                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/FIRE_ROTOR.png'
-                } else if (include.type === 'HELICOPTER' && include.domain === 'LAW') {
-                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/LE_ROTOR.png'
-                } else if (include.type === 'FIXED WING' && include.domain === 'EMS') {
-                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/EMS_FIXED_WING.png'
-                } else if (include.type === 'FIXED WING' && include.domain === 'FIRE') {
-                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/FIRE_AIR_ATTACK.png'
-                } else if (include.type === 'FIXED WING' && include.domain === 'LAW') {
-                    feat.properties.icon = '66f14976-4b62-4023-8edb-d8d2ebeaa336/Public Safety Air/LE_FIXED_WING.png'
-                }
-
-                if (feat.properties.callsign === 'FIREBIRD09-PPD') console.log(new Date(), feat.geometry.coordinates)
-
-                if (!features_ids.has(id)) {
-                    features_ids.add(id);
-                    features.push(feat);
-                }
-            }
-        }
-
-        console.log(`ok - fetched ${ids.size} planes`);
-        const fc = {
-            type: 'FeatureCollection',
-            features
-        };
-
-        await this.submit(fc);
-
-        /*
-        const knownres = await fetch(new URL(`/api/layer/${this.etl.layer}/query`, this.etl.api), {
-            method: 'GET',
-            headers: {
-                Authorization: `bearer ${this.etl.token}`
-            }
-        });
-
-        const known = await knownres.json();
-
-        //TODO: Implement
-        console.log(`ok - comparing against ${known.features.length} planes`);
-        */
     }
 }
 
