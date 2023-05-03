f26252b212f6fd00d3399232f4bd9604ace813a3
ingalls <nick@ingalls.ca>
2023-05-03 14:04:39 -0700

    Add Coordinate Parsing

--- task.ts
+++ task.ts
@@ -1,5 +1,7 @@
 import fs from 'node:fs';
-import { FeatureCollection } from 'geojson';
+import moment from 'moment';
+import { FeatureCollection, Feature, Geometry } from 'geojson';
+import xml2js from 'xml2js';
 import ETL, {
     Event
 } from '@tak-ps/etl';
@@ -12,6 +14,12 @@ try {
     Object.assign(process.env, JSON.parse(String(fs.readFileSync(dotfile))));
 } catch (err) {
     console.log('ok - no .env file loaded');
+}
+
+export interface Share {
+    ShareId: string;
+    CallSign?: string;
+    Password?: string;
 }
 
 export default class Task extends ETL {
@@ -59,14 +67,83 @@ export default class Task extends ETL {
 
         if (!layer.environment.INREACH_MAP_SHARES) throw new Error('No INREACH_MAP_SHARES Provided');
 
+        const obtains: Array<Promise<Feature[]>> = [];
         for (const share of layer.environment.INREACH_MAP_SHARES) {
+            obtains.push((async (share: Share): Promise<Feature[]> => {
+                try {
+                    if (share.ShareId.startsWith('https://')) {
+                        share.ShareId = new URL(share.ShareId).pathname.replace(/^\//, '');
+                    } else if (share.ShareId.startsWith('share.garmin.com')) {
+                        share.ShareId = share.ShareId.replace('share.garmin.com/', '');
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
 
+                if (!share.CallSign) share.CallSign = share.ShareId;
+
+                const url = new URL(`/feed/Share/${share.ShareId}`, 'https://explore.garmin.com')
+                url.searchParams.append('d1', moment().subtract(30, 'minutes').utc().format());
+
+                const kmlres = await fetch(url);
+                const body = await kmlres.text();
+
+                const featuresmap: Map<String, Feature> = new Map();
+                const features: Feature[] = [];
+
+                if (!body.trim()) return features;
+
+                let xml = await xml2js.parseStringPromise(body);
+                if (!xml.kml || !xml.kml.Document) throw new Error('XML Parse Error: Document not found');
+                if (!xml.kml.Document[0] || !xml.kml.Document[0].Folder || !xml.kml.Document[0].Folder[0]) return;
+
+                for (const placemark of xml.kml.Document[0].Folder[0].Placemark) {
+                    if (!placemark.Point || !placemark.Point[0]) break;
+
+                    const coords = placemark.Point[0].coordinates[0].split(',').map((ele: string) => {
+                        return parseFloat(ele);
+                    });
+
+                    const feat: Feature<Geometry, { [name: string]: any; }> = {
+                        id: `inreach-${share.CallSign}`,
+                        type: 'Feature',
+                        properties: {
+                            callsign: share.CallSign,
+                            time: new Date(placemark.TimeStamp[0].when[0]),
+                            start: new Date(placemark.TimeStamp[0].when[0])
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: coords
+                        }
+                    };
+
+                    if (featuresmap.has(String(feat.id))) {
+                        const existing = featuresmap.get(String(feat.id));
+
+                        if (moment(feat.properties.time).isAfter(existing.properties.time)) {
+                            featuresmap.set(String(feat.id), feat);
+                        }
+                    } else {
+                        featuresmap.set(String(feat.id), feat);
+                    }
+                }
+
+                features.push(...Array.from(featuresmap.values()))
+
+                return features;
+            })(share))
         }
 
         const fc: FeatureCollection = {
             type: 'FeatureCollection',
             features: []
         }
+
+        for (const res of await Promise.all(obtains)) {
+            if (!res || !res.length) continue;
+            fc.features.push(...res);
+        };
 
         await this.submit(fc);
     }
